83650ed5e2e8418238bfb12824cc511f3b228726
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-09-19 14:07:58 +0000

    test: Add AuthRedirect unit tests

---
+++ src/components/Contents/Auth/AuthRedirect/AuthRedirect.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { Redirect } from 'react-router-dom';
+
+import { DisconnectedAuthRedirect as AuthRedirect} from './AuthRedirect';
+
+configure({ adapter: new Adapter() });
+
+describe('<AuthRedirect />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<AuthRedirect setContinueUrl={() => {}} />);
+  });
+
+  it('should redirect to continueUrl', () => {
+    const testUrl = '/testurl';
+    wrapper.setProps({ continueUrl: testUrl })
+    expect(wrapper.contains(<Redirect to={testUrl} />)).toBe(true);
+  });
+
+  it('should redirect to / if no continueUrl set', () => {
+    expect(wrapper.find(Redirect).prop('to')).toBe('/');
+  });
+
+  it('should reset the continueUrl to null', () => {
+    const mockFn = jest.fn();
+    wrapper.setProps({ setContinueUrl: mockFn });
+    expect(mockFn).toHaveBeenCalledWith(null);
+  });
+});
