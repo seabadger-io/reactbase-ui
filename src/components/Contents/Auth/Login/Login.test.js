ba82f7463935254c268bd46802b44d7524d3d6eb
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-09-19 14:56:03 +0000

    test: Auth/Login unit tests

---
+++ src/components/Contents/Auth/Login/Login.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import LinerProgress from '@material-ui/core/LinearProgress';
+
+import Login from './Login';
+import firebase from '../../../../firebase';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../../../../firebase', () => {
+  return {
+    auth: {
+      signInWithPopup: jest.fn(() => {
+        return new Promise(resolve => resolve({}));
+      }),
+    },
+    providers: {
+      google: 'google',
+    }
+  };
+});
+
+describe('<Login />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Login />);
+  });
+
+  it('should call firebase signInWithPopup', () => {
+    expect(firebase.auth.signInWithPopup).toHaveBeenCalled();
+  });
+
+  it('should render login in progress message with progress bar', () => {
+    wrapper.setState({ error: null });
+    expect(wrapper.find(Typography).first().render().text()).toEqual('Login in progress...');
+    expect(wrapper.find(LinerProgress).length).toBe(1);
+  });
+
+  it('should display an error message on error', () => {
+    wrapper.setState({ error: 'Error Message' });
+    expect(wrapper.find(Typography).first().render().text()).toEqual('Error Message');
+  });
+
+  it('should render a "Retry login" button on error', () => {
+    wrapper.setState({ error: 'unknownerror' });
+    expect(wrapper.find(Button).first().render().text()).toEqual('Retry login');
+  });
+
+});
