80319cf93d1da0c6c5d2fd02a3e153a845dbe201
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-09-19 15:11:57 +0000

    test: Logout unit tests

---
+++ src/components/Contents/Auth/Logout/Logout.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Typography from '@material-ui/core/Typography';
+import LinerProgress from '@material-ui/core/LinearProgress';
+
+import Logout from './Logout';
+import firebase from '../../../../firebase';
+import * as routes from '../../../ContentRouter/routes';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../../../../firebase', () => {
+  return {
+    auth: {
+      signOut: jest.fn(() => {
+        return new Promise(resolve => resolve({}));
+      }),
+    }
+  };
+});
+
+describe('<Logout />', () => {
+  let wrapper;
+
+  const mockHistory = {
+    push: jest.fn()
+  }
+
+  beforeEach(() => {
+    wrapper = shallow(<Logout history={mockHistory} />);
+  });
+
+  it('should call firebase signOut', () => {
+    expect(firebase.auth.signOut).toHaveBeenCalled();
+  });
+
+  it('should render Logout in progress message with progress bar', () => {
+    expect(wrapper.find(Typography).first().render().text()).toEqual('Logging out...');
+    expect(wrapper.find(LinerProgress).length).toBe(1);
+  });
+
+  it('should redirect after logout', () => {
+    expect(mockHistory.push).toHaveBeenCalledWith(routes.AUTH_REDIRECT);
+  });
+
+});
