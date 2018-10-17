f07935fc4358bc15463a90bae712c8c350611583
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-10-17 14:35:58 +0000

    feat: Add placeholder content to Index

--- src/components/Contents/Index/Index.jsx
+++ src/components/Contents/Index/Index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const style = {
+  root: {
+    width: '80%',
+    maxWidth: '640px',
+    padding: '16px',
+  },
+};
 
 const index = () => (
-  <div />
+  <Paper style={style.root}>
+    <Typography variant="display3">
+      Welcome
+    </Typography>
+    A React application frame with Firebase backend
+  </Paper>
 );
 
 export default index;
